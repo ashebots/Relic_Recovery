7059a97cabaee36b8f96dcd0f9ed4b28d3e0fe8d
GammingCow <secretbuilder00@gmail.com>
2017-09-26 20:35:48 -0400

    Keegan Code
    I changed the code such that it could be controlled by the left joystick.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Move/VueMarkID.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Move/VueMarkID.java
@@ -7,6 +7,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
+import org.firstinspires.ftc.teamcode.Autonomous.Move.ConfigStrings;
 
 
 /**
