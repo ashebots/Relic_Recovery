82442236f997a4d1be9c43089b94304c58a08bc4
GammingCow <secretbuilder00@gmail.com>
2017-11-21 20:49:52 -0500

    modular aton changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/ModularAutonomous/ModularConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/ModularAutonomous/ModularConstants.java
@@ -12,9 +12,17 @@ public class ModularConstants {
     public static float[] BALANCE_STONE_A = {2, 2};
     public static float[] BALANCE_STONE_B = {2, 8};
 
-    public static float[] Left_COLUMN_A = {2, 4.5f};
-    public static float[] Mid_COLUMN_A = {2, 5};
-    public static float[] Right_COLUMN_A = {2, 5.5f};
+    public static float[] LEFT_COLUMN_A = {2, 4.5f};
+    public static float[] MID_COLUMN_A = {2, 5};
+    public static float[] RIGHT_COLUMN_A = {2, 5.5f};
+
+    public static float[] LEFT_COLUMN_B = {2.5f, 10};
+    public static float[] MID_COLUMN_B = {3, 10};
+    public static float[] RIGHT_COLUMN_B = {3.5f, 10};
+
+    //Pictograph scanning positions
+    public static float[] PICTOGRAPH_A = {2, 3};
+    public static float[] PICTOGRAPH_B = {2, 9};
 
     //Encoders per rotation constants
     public static int NEVERREST_20 = 560;
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/ModularTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/ModularTest.java
@@ -1,16 +1,9 @@
 package org.firstinspires.ftc.teamcode.Autonomous;
 
-import com.qualcomm.hardware.bosch.BNO055IMU;
-import com.qualcomm.hardware.motors.NeveRest60Gearmotor;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
-import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.teamcode.Autonomous.ModularAutonomous.ImuChassis;
 import org.firstinspires.ftc.teamcode.Autonomous.ModularAutonomous.ModularConstants;
 
@@ -21,47 +14,16 @@ import org.firstinspires.ftc.teamcode.Autonomous.ModularAutonomous.ModularConsta
 @Autonomous
 public class ModularTest extends LinearOpMode{
 
-    DcMotor leftMotor;
-    DcMotor rightMotor;
-    Servo lTray;
-    Servo rTray;
-
-    BNO055IMU imu;
-
     ImuChassis imuChassis;
-
-    float[] b = {1, 1};
-    float[][] test = {ModularConstants.BALANCE_STONE_A,ModularConstants.Mid_COLUMN_A};
+    HardwareMap map;
 
     public void runOpMode(){
-
-        rightMotor = hardwareMap.dcMotor.get("Right wheel");
-
-        leftMotor = hardwareMap.dcMotor.get("Left wheel");
-        rightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-
-        lTray = hardwareMap.servo.get("Left rotator");
-        rTray = hardwareMap.servo.get("Right rotator");
-        rTray.setDirection(Servo.Direction.REVERSE);
-
-        imu = hardwareMap.get(BNO055IMU.class, "Imu");
-
-        imuChassis = new ImuChassis(leftMotor, rightMotor, imu, 2959);
+        imuChassis = new ImuChassis(map, 2959.0);
         imuChassis.driveSetup(ModularConstants.NEVERREST_40, 1.5f, 4);
-
-        lTray.setPosition(0.8);
-        rTray.setPosition(0.8);
+        float[][] test = {ModularConstants.BALANCE_STONE_A,ModularConstants.MID_COLUMN_A};
 
         waitForStart();
 
         imuChassis.driveToCoords(test, 0.8, 0.3, false);
-
-        imuChassis.turnToAngle(90, .4);
-        imuChassis.driveXFeet(-2, .8);
-        lTray.setPosition(0.4);
-        rTray.setPosition(0.4 );
-
-
-
     }
 }
