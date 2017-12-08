0845cbec01aeb74b1c9624ea21a420eebefd68d8
GammingCow <secretbuilder00@gmail.com>
2017-12-07 20:42:19 -0500

    team code ubdates

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/ModularTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/ModularTest.java
@@ -3,15 +3,12 @@ package org.firstinspires.ftc.teamcode.Autonomous;
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.Autonomous.ModularAutonomous.ImuChassis;
 import org.firstinspires.ftc.teamcode.Autonomous.ModularAutonomous.ModularConstants;
 import org.firstinspires.ftc.teamcode.Autonomous.Move.VueMarkID;
-
 
 @Autonomous
 public class ModularTest extends LinearOpMode{
