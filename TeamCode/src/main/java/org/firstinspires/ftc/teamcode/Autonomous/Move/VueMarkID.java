8f75a07d0cdbec2c4722d239f08d48e33624c0f9
WelfareBread <andrussderp@gmail.com>
2018-01-06 12:49:18 -0500

    Changed camera direction

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Move/VueMarkID.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Move/VueMarkID.java
@@ -25,7 +25,7 @@ public class VueMarkID {
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
         parameters.vuforiaLicenseKey = ConfigStrings.vueforiaKey;
-        parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
+        parameters.cameraDirection = VuforiaLocalizer.CameraDirection.FRONT;
         vuforia = ClassFactory.createVuforiaLocalizer(parameters);
         relicTrackables = vuforia.loadTrackablesFromAsset("RelicVuMark");
         relicTemplate = relicTrackables.get(0);
